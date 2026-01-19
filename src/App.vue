41694e1e07851d1581c838266a9a177ab60338d1
Willon Jiang <1061258787@qq.com>
2026-01-19 15:36:23 +0800

    Updated App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import HelloWorld from "./components/HelloWorld.vue"
+  import TodoList from "./components/TodoList.vue"
 </script>
 
 <template>
   <HelloWorld/>
+  <TodoList/>
 </template>
